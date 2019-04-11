6d9ff239e400eeb4e498862f7638fe4b36014ddf
Adam Stachowicz <adam.stachowicz@hotmail.com>
2019-04-11 12:14:08 +0200

    Transaction Start

---
+++ Lombard.BL/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lombard.BL.Models
+{
+    public class Transaction
+    {
+        public DateTime TransactionDate { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal Price { get; private set; }
+        //public Item Item { get; private set; }
+
+        public decimal GetTransactionAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Lombard.Tests/TransactionTests.cs
@@ -0,0 +1,17 @@
+﻿using Lombard.BL.Models;
+using NUnit.Framework;
+
+namespace Lombard.Tests
+{
+    [TestFixture]
+    public class TransactionTests
+    {
+        [Test]
+        public void GetTransactionAmount_GivenPriceAndQuantity_ShouldReturnProperValue()
+        {
+            var transaction = new Transaction();
+
+            var result = transaction.GetTransactionAmount();
+        }
+    }
+}
