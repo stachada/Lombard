880c6c52798815175e37f9399d026ec4dd23ee8a
Bartłomiej Kacperski <bartlomiejkacperski7734@gmail.com>
2019-04-11 12:14:03 +0200

    Customer basic test and implementation

---
+++ Lombard.BL/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lombard.BL.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public void ChangeName(string newName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAdult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lombard.Tests/CustomerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lombard.BL.Models;
+using NUnit.Framework;
+
+namespace Lombard.Tests
+{
+    class CustomerTests
+    {
+        [Test]
+        public void Can_Change_Name_Test()
+        {
+            //Prepare
+            Customer customer = new Customer()
+            {
+                Name = "Test"
+            };
+            string newName = "NewTest";
+
+            //Act
+            customer.ChangeName(newName);
+
+            //Assert
+            Assert.AreEqual(newName, customer.Name);
+        }
+
+        [Test]
+        public void Is_Adult_Test()
+        {
+            //Prepare
+            Customer customer = new Customer()
+            {
+                BirthDate = new DateTime(1997, 5, 6)
+            };
+
+            //Act
+
+            //Assert
+            Assert.IsTrue(customer.IsAdult());
+        }
+    }
+}
