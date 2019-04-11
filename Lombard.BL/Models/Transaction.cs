5d12a4004773cd08140b7bf429a1760af7255daa
Adam Stachowicz <adam.stachowicz@hotmail.com>
2019-04-11 12:34:17 +0200

    CreateTransactionTest is Green

--- Lombard.BL/Models/Transaction.cs
+++ Lombard.BL/Models/Transaction.cs
@@ -6,10 +6,26 @@ namespace Lombard.BL.Models
 {
     public class Transaction
     {
+        public static Transaction CreateTransaction(Item item, Customer customer, int quantity)
+        {
+            Transaction transaction = new Transaction();
+            transaction.Item = item;
+            transaction.Customer = customer;
+            transaction.Quantity = quantity;
+
+            return transaction;
+        }
+
+        private Transaction()
+        {
+
+        }
+
         public DateTime TransactionDate { get; private set; }
         public int Quantity { get; private set; }
         public decimal Price { get; private set; }
-        //public Item Item { get; private set; }
+        public Item Item { get; private set; }
+        public Customer Customer { get; private set; }
 
         public decimal GetTransactionAmount()
         {
--- Lombard.Tests/TransactionTests.cs
+++ Lombard.Tests/TransactionTests.cs
@@ -7,11 +7,25 @@ namespace Lombard.Tests
     public class TransactionTests
     {
         [Test]
-        public void GetTransactionAmount_GivenPriceAndQuantity_ShouldReturnProperValue()
+        public void CreateTransaction_GivenItemCustomerQuantity_ShouldCreateCorrectCustomer()
         {
-            var transaction = new Transaction();
+            var transaction = Transaction.CreateTransaction(new Item(), new Customer(), 1);
 
-            var result = transaction.GetTransactionAmount();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(transaction.Item, Is.Not.Null);
+                Assert.That(transaction.Customer, Is.Not.Null);
+                Assert.That(transaction.Quantity, Is.EqualTo(1));
+            });
         }
+        
+        //[Test]
+        //public void GetTransactionAmount_GivenPriceAndQuantity_ShouldReturnProperValue()
+        //{
+        //    var transaction = new Transaction();
+
+        //    var result = transaction.GetTransactionAmount();
+        //}
     }
 }
