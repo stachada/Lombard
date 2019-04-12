97a8080123944066af8b2dc8831e502b06ad2576
Adam Stachowicz <adam.stachowicz@hotmail.com>
2019-04-12 13:57:00 +0200

    TransactionService skeleton

---
+++ Lombard.BL/Services/ITransactionService.cs
@@ -0,0 +1,14 @@
+﻿using Lombard.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lombard.BL.Services
+{
+    public interface ITransactionService
+    {
+        Task BuyAsync(int itemId, int customerId, int quantity, decimal price);
+        Task SellAsync(int itemId, int customerId, int quantity, decimal price);
+    }
+}
---
+++ Lombard.BL/Services/TransactionService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Lombard.BL.RepositoriesInterfaces;
+
+namespace Lombard.BL.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private readonly ITransactionsRepository _transactionsRepo;
+        private readonly IItemsRepository _itemsRepo;
+        // private readonly ICustomersRepository _customersRepo;
+
+        public TransactionService(ITransactionsRepository transactionsRepo, IItemsRepository itemsRepo)
+        {
+            _transactionsRepo = transactionsRepo;
+            _itemsRepo = itemsRepo;
+        }
+
+        public async Task BuyAsync(int itemId, int customerId, int quantity, decimal price)
+        {
+            // Get item from database
+            // Get customer from database
+            // Validate transaction
+            // Update quantity on item
+            // Create Transaction
+            // Save Item
+            // Save Transaction
+            throw new NotImplementedException();
+        }
+
+        public async Task SellAsync(int itemId, int customerId, int quantity, decimal price)
+        {
+            // Get item from database
+            // Get customer from database
+            // Validate transaction
+            // Update quantity on item
+            // Create Transaction
+            // Save Item
+            // Save Transaction
+            throw new NotImplementedException();
+        }
+    }
+}
