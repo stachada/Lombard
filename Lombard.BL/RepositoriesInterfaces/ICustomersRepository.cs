1fc24adb641d0ec2fa3a6c04f0ee231da9a2c97a
Bartłomiej Kacperski <bartlomiejkacperski7734@gmail.com>
2019-04-13 11:52:42 +0200

    Customer repository and interface initial implementation

---
+++ Lombard.BL/RepositoriesInterfaces/ICustomersRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lombard.BL.Models;
+
+namespace Lombard.BL.RepositoriesInterfaces
+{
+    public interface ICustomersRepository
+    {
+        IEnumerable<Customer> GetAll();
+        Customer GetCustomerById(int customerId);
+        void AddCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomer(int customerId);
+    }
+}
