de93552ebf2b48b8ef399bcb88d4a5f237b5c86f
Adam Stachowicz <adam.stachowicz@hotmail.com>
2019-04-14 18:10:28 +0200

    ReportService started

---
+++ Lombard.BL/Services/IReportService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lombard.BL.Services
+{
+    public interface IReportService
+    {
+        Task<decimal> GetTurnover(DateTime start, DateTime end);
+        Task<decimal> GetProfit(DateTime start, DateTime end);
+    }
+}
---
+++ Lombard.BL/Services/ReportService.cs
@@ -0,0 +1,30 @@
+﻿using Lombard.BL.RepositoriesInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lombard.BL.Services
+{
+    public class ReportService : IReportService
+    {
+        private readonly ITransactionsRepository _transactionsRepo;
+        private readonly IItemsRepository _itemsRepos;
+
+        public ReportService(ITransactionsRepository transactionsRepo, IItemsRepository itemsRepos)
+        {
+            _transactionsRepo = transactionsRepo;
+            _itemsRepos = itemsRepos;
+        }
+
+        public async Task<decimal> GetProfit(DateTime start, DateTime end)
+        {
+            return await _transactionsRepo.GetProfit(start, end);
+        }
+
+        public async Task<decimal> GetTurnover(DateTime start, DateTime end)
+        {
+            return await _transactionsRepo.GetTurnover(start, end);
+        }
+    }
+}
