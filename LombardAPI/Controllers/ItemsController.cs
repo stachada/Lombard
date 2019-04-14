f0ca31cbc1cb99f9177a87e319d20ff65185006e
Leszek Błażewski <blazewski.leszek@gmail.com>
2019-04-14 18:28:21 +0200

    Aded test for controller and reference for . net core framework in tests.

--- LombardAPI/Controllers/ItemsController.cs
+++ LombardAPI/Controllers/ItemsController.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using AutoMapper;
 using Lombard.BL.Models;
 using Lombard.BL.Services;
 using LombardAPI.Dtos;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LombardAPI.Controllers
