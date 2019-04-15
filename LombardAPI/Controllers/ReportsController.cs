650a494852c039d0e7c6dbf92e6d04884c8ac624
Adam Stachowicz <adam.stachowicz@hotmail.com>
2019-04-15 09:15:24 +0200

    Profit method in ReportsController implemented

--- LombardAPI/Controllers/ReportsController.cs
+++ LombardAPI/Controllers/ReportsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Lombard.BL.Services;
+using LombardAPI.Queries;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,6 +21,14 @@ namespace LombardAPI.Controllers
         {
             _reportService = reportService;
             _mapper = mapper;
+        }
+
+        [HttpGet("profit")]
+        public async Task<ActionResult> Get([FromQuery]PeriodQuery query)
+        {
+            var profit = await _reportService.GetProfit(query.StartDate, query.EndDate);
+
+            return Ok(profit);
         }
     }
 }
---
+++ LombardAPI/Queries/PeriodQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LombardAPI.Queries
+{
+    public class PeriodQuery
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
